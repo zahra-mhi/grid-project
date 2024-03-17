5a48ff0087d698f41eb1df14048545f91554594c
zahra mehri <zahra.s.mehri@gmail.com>
2024-03-17 13:32:23 +0330

    set footer

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const navbar = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
+
 
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
@@ -21,3 +23,6 @@ navBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
     sidebar.classList.remove("show-sidebar");
 });
+
+// set year
+date.innerHTML = new Date().getFullYear();
